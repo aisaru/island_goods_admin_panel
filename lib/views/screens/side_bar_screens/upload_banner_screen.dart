f4c86d7961d36538529474399dcac22770c0f545
aisaru <aaisaru@gmail.com>
2023-06-05 03:27:09 +0500

    first commit

--- lib/views/screens/side_bar_screens/upload_banner_screen.dart
+++ lib/views/screens/side_bar_screens/upload_banner_screen.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 class UploadBannerScreen extends StatefulWidget {
   static const String routeName = '\UploadBannerScreen';
@@ -9,7 +12,12 @@ class UploadBannerScreen extends StatefulWidget {
 }
 
 class _UploadBannerScreenState extends State<UploadBannerScreen> {
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
   dynamic _image;
+
+  String? fileName;
 
   pickImage() async {
     FilePickerResult? result = await FilePicker.platform
@@ -18,6 +26,32 @@ class _UploadBannerScreenState extends State<UploadBannerScreen> {
     if (result != null) {
       setState(() {
         _image = result.files.first.bytes;
+
+        fileName = result.files.first.name;
+      });
+    }
+  }
+
+  _uploadBannersToStorage(dynamic image) async {
+    Reference ref = _storage.ref().child('Banners').child(fileName!);
+
+    UploadTask uploadTask = ref.putData(image);
+
+    TaskSnapshot snapshot = await uploadTask;
+
+    String downloadUrl = await snapshot.ref.getDownloadURL();
+    return downloadUrl;
+  }
+
+  uploadToFireStore() async {
+    EasyLoading.show();
+    if (_image != null) {
+      String imageUrl = await _uploadBannersToStorage(_image);
+
+      await _firestore.collection('banners').doc(fileName).set({
+        'image': imageUrl,
+      }).whenComplete(() {
+        EasyLoading.dismiss();
       });
     }
   }
@@ -31,7 +65,7 @@ class _UploadBannerScreenState extends State<UploadBannerScreen> {
             alignment: Alignment.topLeft,
             padding: const EdgeInsets.all(10),
             child: const Text(
-              'Banners',
+              'Banner',
               style: TextStyle(
                 fontWeight: FontWeight.w700,
                 fontSize: 36,
@@ -55,9 +89,14 @@ class _UploadBannerScreenState extends State<UploadBannerScreen> {
                         border: Border.all(color: Colors.grey.shade900),
                         borderRadius: BorderRadius.circular(10),
                       ),
-                      child: Center(
-                        child: Text('Banners'),
-                      ),
+                      child: _image != null
+                          ? Image.memory(
+                              _image,
+                              fit: BoxFit.cover,
+                            )
+                          : Center(
+                              child: Text('Banner'),
+                            ),
                     ),
                     SizedBox(
                       height: 20,
@@ -79,7 +118,9 @@ class _UploadBannerScreenState extends State<UploadBannerScreen> {
               ElevatedButton(
                 style:
                     ElevatedButton.styleFrom(primary: Colors.yellow.shade900),
-                onPressed: () {},
+                onPressed: () {
+                  uploadToFireStore();
+                },
                 child: Text('Save'),
               ),
             ],
