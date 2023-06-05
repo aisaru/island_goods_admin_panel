83de8b8007a8437f8c41583913eb8d3daebadb2d
aisaru <aaisaru@gmail.com>
2023-06-05 08:45:44 +0500

    another commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_admin_scaffold/admin_scaffold.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
-import 'package:island_goods_web_admin/views/screens/main_screen.dart';
+
+import 'views/dashboard_screens.dart/dashboard_screens.dart';
+import 'views/screens/category_screen.dart';
+import 'views/screens/order_screen.dart';
+import 'views/screens/product_screen.dart';
+import 'views/screens/upload_banner_screen.dart';
+import 'views/screens/vendors_screen.dart';
+import 'views/screens/withdrawal_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,13 +39,154 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Admin Panel',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.cyan,
       ),
-      home: MainScreen(),
+      home: SideMenu(),
       builder: EasyLoading.init(),
+    );
+  }
+}
+
+class SideMenu extends StatefulWidget {
+  static const String id = '\sideMenu';
+
+  @override
+  State<SideMenu> createState() => _SideMenuState();
+}
+
+class _SideMenuState extends State<SideMenu> {
+  Widget _selectedScreen = DashboardScreen();
+
+  screnSelectore(item) {
+    switch (item.route) {
+      case CategoryScreen.id:
+        setState(() {
+          _selectedScreen = CategoryScreen();
+        });
+
+        break;
+      case VendorsScreen.id:
+        setState(() {
+          _selectedScreen = VendorsScreen();
+        });
+
+        break;
+
+      case WithdrawalScreen.id:
+        setState(() {
+          _selectedScreen = WithdrawalScreen();
+        });
+
+        break;
+
+      case OrderScreen.routeName:
+        setState(() {
+          _selectedScreen = OrderScreen();
+        });
+
+        break;
+
+      case UploadBanners.id:
+        setState(() {
+          _selectedScreen = UploadBanners();
+        });
+
+        break;
+
+      case ProductScreen.routeName:
+        setState(() {
+          _selectedScreen = ProductScreen();
+        });
+
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AdminScaffold(
+      backgroundColor: Color.fromRGBO(255, 255, 255, 1),
+      appBar: AppBar(
+        backgroundColor: Colors.yellow.shade900,
+        title: const Text(
+          'Management',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      sideBar: SideBar(
+        items: const [
+          AdminMenuItem(
+            title: 'Vendors',
+            route: VendorsScreen.id,
+            icon: CupertinoIcons.person_3,
+          ),
+          AdminMenuItem(
+            title: 'Withdrawal',
+            route: WithdrawalScreen.id,
+            icon: CupertinoIcons.money_dollar,
+          ),
+          AdminMenuItem(
+            title: 'Orders',
+            route: OrderScreen.routeName,
+            icon: CupertinoIcons.shopping_cart,
+          ),
+          AdminMenuItem(
+            title: 'Categories',
+            icon: Icons.category_rounded,
+            route: CategoryScreen.id,
+          ),
+          AdminMenuItem(
+            title: 'Upload Banner',
+            icon: CupertinoIcons.add,
+            route: UploadBanners.id,
+          ),
+          AdminMenuItem(
+            title: 'Products',
+            icon: CupertinoIcons.shopping_cart,
+            route: ProductScreen.routeName,
+          ),
+        ],
+        selectedRoute: VendorsScreen.id,
+        onSelected: (item) {
+          screnSelectore(item);
+
+          // if (item.route != null) {
+          //   Navigator.of(context).pushNamed(item.route!);
+          // }
+        },
+        header: Container(
+          height: 50,
+          width: double.infinity,
+          color: Colors.yellow.shade700,
+          child: const Center(
+            child: Text(
+              'Multi Store Admin ',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+        footer: Container(
+          height: 50,
+          width: double.infinity,
+          color: const Color(0xff444444),
+          child: const Center(
+            child: Text(
+              'footer',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: _selectedScreen,
     );
   }
 }
